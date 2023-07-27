dc3e490b687c1110d8773497379f92269bd5de48
unnamedAniki <67883330+unnamedAniki@users.noreply.github.com>
2023-07-28 02:17:36 +0300

    Some translates

--- cities/weather/models.py
+++ cities/weather/models.py
@@ -7,15 +7,13 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
-    def __int__(self):
-        return self.id
-
     name = models.CharField(max_length=255)
 
 
 class WeatherValue(models.Model):
     def __str__(self):
         return f"city: {self.cities.name}, temp: {self.value}C"
+
     date = models.DateTimeField(default=datetime.datetime.now())
     value = models.FloatField(default=0)
     cities = models.ForeignKey(City, on_delete=models.CASCADE)
