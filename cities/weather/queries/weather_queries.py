31df0afa408ba95d760da869134b650a0d210a94
unnamedAniki <67883330+unnamedAniki@users.noreply.github.com>
2023-07-29 00:27:14 +0300

    Added extra filter to weather page
    Added action button in citiesView - this perform an action to show weather records with selected city

--- cities/weather/queries/weather_queries.py
+++ cities/weather/queries/weather_queries.py
@@ -10,6 +10,11 @@ def get_weather_by_city_and_date(city_id: int, date: datetime):
         filter(cities_id=city_id, date__range=(date, date + datetime.timedelta(days=1)))
 
 
+def get_weather_by_city(city_id: int):
+    return models.WeatherValue.objects.\
+        filter(cities_id=city_id)
+
+
 def get_single_weather_by_city_and_date(city_id: int, date: datetime, weather_id: int = 0):
     return models.WeatherValue.objects.\
         filter(cities_id=city_id, date=date).exclude(id=weather_id)
